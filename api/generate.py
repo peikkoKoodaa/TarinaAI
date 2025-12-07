7b6b9f7e6eb8a400339f3e42d97ccbf1233f3c33
peikkoKoodaa <toivo.i.rytkonen@gmail.com>
2025-12-07 14:53:00 +0200

    Update generate.py

--- api/generate.py
+++ api/generate.py
@@ -1,1 +1,14 @@
+def handler(request):
+    import os
 
+    here = os.path.dirname(__file__)
+    tarinat_path = os.path.join(here, "..", "tarinat.txt")
+
+    with open(tarinat_path, "r", encoding="utf-8") as f:
+        tarinat = f.read()
+
+    # Palaa JSON-vastauksella
+    return {
+        "status": 200,
+        "body": tarinat
+    }
